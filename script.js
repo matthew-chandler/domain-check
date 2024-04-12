498d2ace5c4c99951a0145860b17585d730caf7b
Matthew Chandler <48606413+matthew-chandler@users.noreply.github.com>
2024-04-12 01:13:37 -0700

    check alternative domains

--- script.js
+++ script.js
@@ -10,51 +10,70 @@ fetch('TLDs.json')
     .then(response => response.json())
     .then(data => {
         TLDs.push(...data);
-        console.log(TLDs);
     });
 
-
 async function checkDomainAvailability() {
-    resultsContainer.innerHTML = ''; // Clear previous results
+    resultsContainer.innerHTML = ''; // clear previous results
     const domains = domainInput.value.split('\n').filter(domain => domain.trim() !== '');
     
+    // ensure at least one domain was entered
     if (domains.length === 0) {
         resultsContainer.innerHTML = '<p>Please enter some domains to check.</p>';
         return;
     }
 
+    // add alternative domains
+    const altDomains = [];
+    for (const name of domains) {
+        const SLD = name.split(".")[0];
+        const satisfiedEndings = TLDs.filter(ending => SLD.endsWith(ending.replace(".","")));
+        for (const ending of satisfiedEndings)
+            altDomains.push(SLD.slice(0,-ending.replace(".","").length)+ending);
+    }
+    const domainString = (domains.concat(altDomains)).join(",");
+
     const availableDomains = [];
     const unavailableDomains = [];
-    const invalidDomains = [];
+    const alternativeDomains = [];
+    const invalidDomains = [];    
 
     checkButton.disabled = true; // Disable button during API request
     try {
-        const domainString = domains.join(",");
         const response = await fetch(`https://api.matthewachandler.com/domains?domain=${domainString}`);
         if (!response.ok) {
             throw new Error('Failed to fetch domain availability.');
         }
         const data = await response.json();
 
+        // handle valid domains
         for (const domain of data.status) {
-            const name = domain.domain
-            const resultElement = document.createElement('p');
-
-            const mainAvailable = domain.summary === 'inactive';
+            const name = domain.domain            
             
-            if (mainAvailable) {
-                availableDomains.push(name);
-            } else {
-                unavailableDomains.push(name);
+            // check if the domain is registerable to the general public and available on namecheap
+            const registerable = TLDs.some(ending => name.endsWith(ending));
+            const mainAvailable = domain.summary === 'inactive';
+            const isAlternative = altDomains.includes(name);
+
+            // handle alternative domains
+            if (isAlternative) {
+                if (mainAvailable && registerable)
+                    alternativeDomains.push(name);
+            } else { // handle regular domains
+                if (mainAvailable && registerable)
+                    availableDomains.push(name);
+                else
+                    unavailableDomains.push(name);
             }
         }
-        for (const domain of data.errors) {
-            const domainInvalid = domain.code === 404;
-            const name = domain.detail;
-            if (domainInvalid) {
-                invalidDomains.push(name);
+        // handle invalid domains
+        if (data.errors)
+            for (const domain of data.errors) {
+                const domainInvalid = domain.code === 404;
+                const name = domain.detail;
+                if (domainInvalid && !altDomains.includes(name)) {
+                    invalidDomains.push(name);
+                }
             }
-        }
     } catch (error) {
         resultsContainer.innerHTML = `<p>An error occurred: ${error.message}</p>`;
     } finally {
@@ -62,15 +81,57 @@ async function checkDomainAvailability() {
     }
 
     for (const domain of availableDomains) {
+        // checks alternatives
+        const allVariations = [domain];
+        const SLD = domain.split(".")[0];
+        const satisfiedEndings = TLDs.filter(ending => SLD.endsWith(ending.replace(".","")));
+        for (const ending of satisfiedEndings) {
+            const altDomain = SLD.slice(0,-ending.replace(".","").length)+ending;
+            if (alternativeDomains.includes(altDomain))
+                allVariations.push(altDomain);
+        }
+
         const resultElement = document.createElement('p');
-        resultElement.textContent = `${domain} is available`;
+        if (allVariations.length === 1)
+            resultElement.textContent = `${allVariations[0]} is available`;
+        else if (allVariations.length === 2)
+            resultElement.textContent = `${allVariations[0]}, as well as ${allVariations[1]} are available`;
+        else if (allVariations.length === 3)
+            resultElement.textContent = `${allVariations[0]}, as well as ${allVariations[1]} and ${allVariations[2]} are available`;
+        else if (allVariations.length >= 4) {
+            const alternativeString = allVariations.slice(1,allVariations.length-1).join(", ") + ", and " + allVariations[allVariations.length-1];
+             resultElement.textContent = `${allVariations[0]}, as well as ${alternativeString} are available`;
+        }    
         resultElement.classList.add('available');
         resultsContainer.appendChild(resultElement);
     }
     for (const domain of unavailableDomains) {
+        // checks alternatives
+        const allVariations = [domain];
+        const SLD = domain.split(".")[0];
+        const satisfiedEndings = TLDs.filter(ending => SLD.endsWith(ending.replace(".","")));
+        for (const ending of satisfiedEndings) {
+            const altDomain = SLD.slice(0,-ending.replace(".","").length)+ending;
+            if (alternativeDomains.includes(altDomain))
+                allVariations.push(altDomain);
+        }
+        
         const resultElement = document.createElement('p');
-        resultElement.textContent = `${domain} is not available `;
+        resultElement.textContent = `${allVariations[0]} is not available` + (allVariations.length !== 1 ? ", " : "");
         resultElement.classList.add('unavailable');
+        if (allVariations.length !== 1) {
+            const alternativeText = document.createElement('span');
+            if (allVariations.length === 2)
+                alternativeText.textContent = `but ${allVariations[1]} is`;
+            else if (allVariations.length === 3)
+                alternativeText.textContent = `but ${allVariations[1]} and ${allVariations[2]} are`;
+            else if (allVariations.length >= 4) {
+                const alternativeString = allVariations.slice(1,allVariations.length-1).join(", ") + ", and " + allVariations[allVariations.length-1];
+                alternativeText.textContent = `but ${alternativeString} are`;
+            }    
+            alternativeText.classList.add('alternatives');
+            resultElement.appendChild(alternativeText);
+        }        
         resultsContainer.appendChild(resultElement);
     }
     for (const domain of invalidDomains) {
